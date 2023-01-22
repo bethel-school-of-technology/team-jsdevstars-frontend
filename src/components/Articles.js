9529a2d9b4a80c94003a155021fb7091bdc3ab11
Felipe Sarres <sarres@gmail.com>
2023-01-22 10:37:10 -0600

    added login page

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -93,7 +93,29 @@ function Articles() {
       <div className='main-container'>
         <Container>
           <img src="https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2hhdCUyMHJvb218ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60" alt="people chatting" />
+          <h3>Heading for first image</h3>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat excepturi dolorem atque pariatur earum voluptates dolores soluta reprehenderit, molestiae, dignissimos inventore tenetur quas magni, porro nesciunt velit similique maiores sint aspernatur tempora iure nisi odio. Impedit omnis dolorum natus quasi! Ea voluptate voluptas labore voluptatibus dolorum magnam quidem explicabo culpa?</p>
+        </Container>
+      </div>
+      <div className='main-container'>
+        <Container>
+          <img src="https://plus.unsplash.com/premium_photo-1663134237332-1aac08064783?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzd8fHRlYW0lMjB3b3JraW5nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60" alt="people running" />
+          <h3>Heading for second image</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat excepturi dolorem atque pariatur earum voluptates dolores soluta reprehenderit, molestiae, dignissimos inventore tenetur quas magni, porro nesciunt velit similique maiores sint aspernatur tempora iure nisi odio. Impedit omnis dolorum natus quasi! Ea voluptate voluptas labore voluptatibus dolorum magnam quidem explicabo culpa?</p>
+        </Container>
+      </div>
+      <div className='main-container'>
+        <Container>
+          <img src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8dGVhbSUyMHdvcmtpbmd8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60" alt="adults chatting" />
+          <h3>Heading for third image</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat excepturi dolorem atque pariatur earum voluptates dolores soluta reprehenderit, molestiae, dignissimos inventore tenetur quas magni, porro nesciunt velit similique maiores sint aspernatur tempora iure nisi odio. Impedit omnis dolorum natus quasi! Ea voluptate voluptas labore voluptatibus dolorum magnam quidem explicabo culpa?</p>
+        </Container>
+      </div>
+      <div className='main-container'>
+        <Container>
+          <img src="https://images.unsplash.com/photo-1607863680198-23d4b2565df0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGZpbmFuY2lhbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60" alt="adults chatting" />
+          <h3>Heading for fourth image</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat excepturi dolorem atque pariatora iure nisi odio. Impedit omnis dolorum natus quasi! Ea voluptate voluptas labore voluptatibus dolorum magnam quidem explicabo culpa?</p>
         </Container>
       </div>
     </div>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import '../styles/Login.css'
 
 function Login() {
+  const submitText = "Let's go!";
   return (
     <div>
-      <h1>This is the login page</h1>
+      <Form className='login'>
+        <Form.Group className="login" controlId="formHorizontalEmail">
+          <Form.Label className='login'>Email address</Form.Label>
+          <Form.Control type="email" placeholder="example@forDads.com" />
+        </Form.Group>
+        <Form.Group className="login" controlId="formHorizontalPassword">
+          <Form.Label className='login'>Password</Form.Label>
+          <Form.Control type="password" placeholder="Enter password" />
+        </Form.Group>
+        <Button className="btn-login" variant="primary" type="submit">
+          {submitText}
+        </Button>
+      </Form>
     </div>
-  )
+  );
 }
 
 export default Login;
