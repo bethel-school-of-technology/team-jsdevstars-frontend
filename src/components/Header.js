209ec8df3cd70c194660764e3ab382fc2f6219bf
Felipe Sarres <sarres@gmail.com>
2023-01-22 18:26:20 -0600

    changes to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,27 @@
 import React from "react";
-import '../styles/Header.css'
-
+import '../styles/Header.css';
+import { Link, Routes, Route } from "react-router-dom";
+import { Nav } from 'react-bootstrap';
+import Signup from "./Signup";
+import Login from "./Login";
 
 function Header() {
   return (
-    <div className="header">
-      <h1>For dads</h1>
-</div>
+    <>
+      <div className="header">
+        <h1 className="h1-header">For dads</h1>
+        <Nav>
+          <Link className="header nav-link text-white" to="/signup"><h3>Sign up</h3></Link>
+          <Link className="header nav-link text-white" to="/login"><h3>Log in</h3></Link>
+        </Nav>
+      </div>
+      <div>
+        <Routes>
+          <Route path='/signup' element={<Signup />} />
+          <Route parth='/login' element={<Login />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
